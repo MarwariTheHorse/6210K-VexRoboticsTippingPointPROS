793f261aab1ebd9892ccfadf3aa090341c540614
Caleb Buening <caleb.j.buening@gmail.com>
2021-06-03 17:44:27 -0400

    Added a bit of layout in the code and deleted old, useless seudocode

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 6210K-VexRoboticsTippingPointPROS
 The amazing code repo for all the PROS code of 2021-2022.
 
+Currently this code is written for a robot with a 6 motor drive + a ring intake and a ring director + pneumatics for grabbing two cones. If this does not end up being the final plan, at least we will have some code to use.
+
 The Plans for this season:
 Fall: Build the robot, (Base>Mechs>Tuning), Program the foundation to the program
 Winter: Caleb works on neural network, building it up from the ground, and other enhancements while Joey puts the auton together. Builders fix things.
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,21 @@
 #define TILT_LEFT 7
 #define TILT_RIGHT 8
 
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor left_mtr(1);
+pros::Motor right_mtr(2);
+
+bool disableControllerScreenLoop = false;
+
+const bool DEBUG = false;
+
+/**
+ * A callback for LLEMU's left button.
+ */
+void on_left_button() {
+    
+}
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -27,10 +42,14 @@ void on_center_button() {
 }
 
 /**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
+ * A callback for LLEMU's right button
+ */
+void on_right_button() {
+    
+}
+
+/**
+ * Runs when program is started. Blocks everything else.
  */
 void initialize() {
 	pros::lcd::initialize();
@@ -40,54 +59,29 @@ void initialize() {
 }
 
 /**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
+ * Runs when something is disabling the robot following either autonomous or opcontrol. Exits when re-enabled.
  */
 void disabled() {}
 
 /**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
+ * Runs after initialize(), and before autonomous when connected to a controller. 
  *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+ * For an auton selector
+ *
+ * Exits when the robot is enabled
  */
 void competition_initialize() {}
 
 /**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
+ * Runs for auton. Can be called for testing purposes.
  */
 void autonomous() {}
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * For operator control. Automatically runs after initialize if not connected 
+ * to a field controller or etc.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
